286b2d61d468b3c8875d07659a1e5ce4908019a9
MohiuddinRabby <rabbymohiuddin@gmail.com>
2021-09-03 19:02:32 +0600

    array

---
+++ w3/08_array.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<array>
+#include<string>
+using namespace std;
+int main()
+{
+    string cars[] = {"vlovo","lancer","bmw","toyota"};
+    for(int i=0; i<cars[i].length(); i++)
+    {
+        cout << cars[i]<<"\n";
+    }
+    return 0;
+}
